d3b02d9cc3d2b9426b04feeaa719a70c59c40eea
axeinstd <axeinstd@gmail.com>
2024-05-01 20:41:19 +0300

    Add required files
    Required files for site

---
+++ index.php
@@ -0,0 +1,287 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>FletChatAI - The AI companion</title>
+<link rel="icon" >
+<meta charset="utf-8">
+<meta name="keywords" content="flet, open-source">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+</head>
+<body>
+<img src="/img/graphics/landing/hero-main.png" width="1" height="1" alt="preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/home/inc-home-player.php'?>
+<div class="page-con-content landing-con-override">
+	<div class="wavebar-con-container-master">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>The Open-source<br>
+							 AI copilot</h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+								 Flet Chat AI is a AI program built on flet, the python framework which use Flutter wigets to render interface.
+							</p>
+							<div class='landing-btn-container'>
+								<div class="build-btn-button">
+									<div class="build-ico-button">
+										<div class="build-ico-os">
+											<img alt="Windows" src="img/icons/list/os-windows-11.png" style='height: 100%; width: 100%; object-fit: contain'/>
+											<img alt="GitHub" src="img/icons/list/github-mark.png" style='height: 100%; width: 100%; object-fit: contain; display:none;'/>
+										</div>
+									</div>
+									<a href="https://github.com/axeinstd/Flet-Chat-AI/releases">
+									<div class="build-tx1-button">
+										<span>Download</span>
+										<span class="build-tx2-label">64-bit</span>
+									</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(/img/graphics/landing/heroZ.png) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-handheld darkmode-feature">
+		<div class="handheld-img-backdrop">
+		</div>
+		<div class="handheld-img-overlay">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="video-tx1-heading darkmode-txt">
+					<h2>Ask anything.</h2>
+				</div>
+				<div class="video-con-divider">
+				</div>
+				<div class="handheld-con-container">
+					<div class="handheld-con-screen">
+						<div class="handheld-img-screen">
+							<img alt="1" src="img/graphics/landing/handheld-1.png" style='height: 100%; width: 100%; object-fit: contain'/>
+							<img alt="2" src="img/graphics/landing/handheld-2.png" style='height: 100%; width: 100%; object-fit: contain; display:none;'/>
+						</div>
+					</div>
+					<div class="handheld-txt-container">
+						<div class="handheld-tx1-title darkmode-txt">
+							<span>Flet Chat AI contains models for all occasions</span>
+						</div>
+						<div class="handheld-tx2-desc darkmode-txt">
+							<span>Flet Chat AI 1.0 is pictured above. Please note that other versions may differ from the picture.</span>
+						</div>
+						<a href="https://github.com/axeinstd/Flet-Chat-AI">
+						<div class="handheld-btn-button">
+							<div class="handheld-ico-button" img>
+							</div>
+							<div class="handheld-tx1-button">
+								<span>Get started with llama3-70b*</span>
+							</div>
+						</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="landing-con-discord darkmode-discord">
+	<div class="discord-img-overlay">
+		</div>
+		<div class="discord-left-logo">
+			<img alt="Discord Left Banner" src="/img/graphics/svg/discord-left.svg" style="width: 100%; height: 100%;">
+		</div>
+		<div class="discord-center-logo">
+			<img alt="Discord Logo" src="/img/graphics/svg/discord-center.svg" style="width: 100%; height: 100%;">
+		</div>
+		<div class="discord-right-logo">
+			<img alt="Discord Right Banner" src="/img/graphics/svg/discord-right.svg" style="width: 100%; height: 100%;">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="discord-con-container">
+					<div class="discord-con-logo">
+						<div class="discord-img-logo">
+						</div>
+					</div>
+					<div class="discord-con-mantra">
+						<div class="discord-wrp-mantra scale-content-txt-1">
+							<div class="discord-tx1-mantra darkmode-txt">
+								<h2>Join the Discord.</h2>
+								<div class="landing-con-divider">
+								</div>
+							</div>
+							<div class="discord-tx2-mantra darkmode-txt">
+								<p>
+									 We use Discord to house our community. Discord allows us to keep our conversations open and accessible. This applies to users new and old who wish to learn how to use, stay up to date and aid to the project's development.
+								</p>
+							</div>
+							<a href="https://discord.gg/9Hk63YHa3N" target="_blank">
+							<div class="discord-btn-button">
+								<div class="discord-ico-button">
+								</div>
+								<div class="discord-tx1-button">
+									<span>Join the discussion</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<svg class="discord-con-animate" preserveaspectratio="none" width="1920" height="100%" viewbox="0 0 1920 330">
+		<path class="discord-svg-animate" fill="#fff" fill-opacity="0.2" d="M140.881198,194.260295 C257.600568,129.32862 342.939626,119.84993 418.009939,203.154617 C493.080251,286.459305 545.728689,70.9046172 636.439626,63.9593047 C727.150564,57.0139922 768.99822,139.670242 858.802907,119.431961 C948.607595,99.1936797 1071.91228,-32.9977266 1243.91228,7.75227342 C1415.91228,48.5022734 1404.10369,208.584305 1508.27166,178.709305 C1612.43963,148.834305 1633.73291,79.913472 1711.63588,98.8569055 C1776.28676,114.577866 1819.96778,221.391836 1889.37253,185.808108 C2017.32661,120.206212 2004.01952,336.769569 2004.01952,336.769569 L271.635881,337 L-149.063338,337 C-149.063338,337 -245.850307,175.637635 -58.0633382,228.867188 C33.8652851,254.92501 64.1722713,236.933925 140.881198,194.260295 Z"></path>
+		</svg>
+	</div>
+	<div class="landing-con-contribute darkmode-github">
+		<div class="github-img-backdrop">
+		</div>
+		<div class="github-img-overlay">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div class="github-con-logo">
+						<div class="github-img-logo">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra">
+								<h2>Contributing Code.</h2>
+								<div class="landing-con-divider" style="background: #9647c7">
+								</div>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+									My project is very simple now, so if you want improve it you can improve it! Source code is always in the releases: source-code.zip. But, pleas, dont forget to comply with the Apache License 2.0. :)
+								</p>
+							</div>
+							<a href="https://github.com/axeinstd/Flet-Chat-AI" target="_blank">
+							<div class="github-btn-button">
+								<div class="github-ico-button">
+								</div>
+								<div class="github-tx1-button">
+									<span>Join the project</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-patreon">
+		<div class="patreon-img-backdrop">
+		</div>
+		<div class="patreon-img-overlay">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="yt-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra">
+								<h2>axeinstd on youtube!</h2>
+								<div class="landing-con-divider" style="background: #ff424d">
+								</div>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									Probably, I will publish video about developing FletChatAI. Anyway, subscribe! (pls)
+								</p>
+							</div>
+							<a href="https://youtube.com/@axeinstd" target="_blank">
+							<div class="patreon-btn-button">
+								<div class="patreon-ico-button">
+								</div>
+								<div class="patreon-tx1-button">
+									<span>Subscribe!</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-patreon">
+		<div class="patreon-img-backdrop-2">
+		</div>
+		<div class="patreon-img-overlay">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patreon-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra">
+								<h2>We are on DonationAlerts.</h2>
+								<div class="landing-con-divider" style="background: #ff424d">
+								</div>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									Today everyone wants to get some money for his work :)
+								</p>
+							</div>
+							<a href="https://www.donationalerts.com/r/axeinstd" target="_blank">
+							<div class="patreon-btn-button">
+								<div class="patreon-ico-button">
+								</div>
+								<div class="patreon-tx1-button">
+									<span>Donate!</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>
